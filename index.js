a875f47d0e6570aba46b1d5da607cb9f4c287a86
briansanchez1 <sanchezmelob@wit.edu>
2023-05-22 15:48:45 -0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var menuSections = document.querySelectorAll('.menu-section');
 
 menuSections.forEach(function (menuSection) {
-  menuSection.addEventListener('mouseover', function () {
-    this.classList.add('active');
+  menuSection.addEventListener('click', function () {
+    if (this.classList.contains('active')) {
+      this.classList.remove('active');
+    } else {
+      this.classList.add('active');
+    }
   });
 });
