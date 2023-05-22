2c6ab1e42b1b6f478962a01d1256e1885ade5fc4
briansanchez1 <sanchezmelob@wit.edu>
2023-05-22 15:49:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ menuSections.forEach(function (menuSection) {
   menuSection.addEventListener('click', function () {
     if (this.classList.contains('active')) {
       this.classList.remove('active');
+      this.classList.add('inactive');
     } else {
+      this.classList.remove('inactive');
       this.classList.add('active');
+
     }
   });
 });
